351ea49b502404b3b401c61020980b87f0a02d8d
Gabriela <gabyplasencia1995@gmail.com>
2024-02-25 17:37:45 +0000

    fix mistake on props practices

---
+++ contact-cat-props/components/Contact.js
@@ -0,0 +1,35 @@
+import React from "react"
+
+export default function Contact(props) {
+    return (
+        <div className="contact-card">
+            <img src={props.img}/>
+            <h3>{props.name}</h3>
+            <div className="info-group">
+                <img src="./images/phone-icon.png" />
+                <p>{props.phone}</p>
+            </div>
+            <div className="info-group">
+                <img src="./images/mail-icon.png" />
+                <p>{props.email}</p>
+            </div>
+        </div>
+    )
+}
+
+// export default function Contact({img, name, phone, email}) <--- Destructuring props {
+//     return (
+//         <div className="contact-card">
+//             <img src={img}/>
+//             <h3>{name}</h3>
+//             <div className="info-group">
+//                 <img src="./images/phone-icon.png" />
+//                 <p>{phone}</p>
+//             </div>
+//             <div className="info-group">
+//                 <img src="./images/mail-icon.png" />
+//                 <p>{email}</p>
+//             </div>
+//         </div>
+//     )
+// }
